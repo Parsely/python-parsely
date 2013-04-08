477d4c7a40ab93e4359eaca8e3b9a78dc325993e
Emmett Butler <emmett.butler321@gmail.com>
2013-04-08 11:55:16 -0400

    add requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='python-parsely',
-    version='1.0',
+    version='1.2',
     py_modules=['parsely'],
     author='emmett butler',
     author_email='emmett.butler321@gmail.com',
-    url='http://github.com/emmett9001/python-parsely'
+    url='http://github.com/emmett9001/python-parsely',
+    requires=['requests']
 )
