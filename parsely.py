d0f93bce993e73ce0dd7a3a0f0b23ff470c079ad
Emmett Butler <emmett.butler321@gmail.com>
2013-04-08 13:13:15 -0400

    copyright

--- parsely.py
+++ parsely.py
@@ -1,5 +1,5 @@
 """
-    Copyright (C) 2013 Emmett Butler
+    Copyright (C) 2013 Emmett Butler, Parsely Inc.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
